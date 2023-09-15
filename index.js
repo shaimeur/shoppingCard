c7c25915727cec73ceb9084d3e97b2b0603e957b
shaimeur <haimeursaad@gmail.com>
2023-09-15 01:06:57 +0100

    adding the side navbar in html istead of js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll('.btn');
-const card = document.querySelector('.card[data-descr]')
+const card = document.querySelector('.card[data-descr]');
+const items = document.querySelector(".items")
+// const sidNav = document.createElement('div')
 let val = 1 ;
 
 
@@ -7,8 +9,19 @@ let val = 1 ;
 
 for (const button of buttons) {
     button.addEventListener('click',function(){
-        console.dir(card.getAttribute('data-descr'))
+        // console.dir(card.getAttribute('data-descr'))
          card.setAttribute('data-descr',val++)
 
     })
 }
+// const markup  =
+//  `<div class="side-nav">
+//         <div class="exits">
+//             <div type="button" class="sum"></div>
+//             <div type="button" class="close">close</div>
+//         </div>
+//  </div>`;
+// sidNav.innerHTML = markup
+
+
+// items.insertAdjacentHTML('beforeend',markup)
