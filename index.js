26e64414701c2f178bc414796c77afd17f174b1f
shaimeur <haimeursaad@gmail.com>
2023-09-15 01:30:09 +0100

    adding the toggle feature to hide the checkout items

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 const buttons = document.querySelectorAll('.btn');
 const card = document.querySelector('.card[data-descr]');
-const items = document.querySelector(".items")
-// const sidNav = document.createElement('div')
+const items = document.querySelector(".items");
+const panier = document.querySelector("#panier")
+const checkout = document.querySelector(".side-nav")
 let val = 1 ;
 
 
+card.addEventListener('click',function() {
+    // console.log(event.target)
+    checkout.classList.remove("title-card","close","sum")
+    checkout.classList.toggle("side-nav")
+    // checkout.classList.toggle("title-card")
+    // checkout.classList.toggle("close")
+    // checkout.classList.toggle("sum")
+})
 
 
 for (const button of buttons) {
     button.addEventListener('click',function(){
         // console.dir(card.getAttribute('data-descr'))
-         card.setAttribute('data-descr',val++)
+        card.setAttribute('data-descr',val++)
 
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SIDE NAV BAR CODE IN JS
+
+// const sidNav = document.createElement('div')
 // const markup  =
 //  `<div class="side-nav">
 //         <div class="exits">
